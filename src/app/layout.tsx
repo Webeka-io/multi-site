3a8896246ce67402dac5f56413c4b0ea066bfbd0
Wylen <webeka.contact@gmail.com>
2025-08-30 22:40:31 +0200

    Mise à jour Analytics / SpeedInsights  

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import "./globals.css";
 import React from "react";
 import { Toaster } from "@/app/components/ui/sonner"; // ou "@/components/ui/toaster" selon la version générée
-
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       {/* on laisse simple pour éviter tout mismatch */}
       <body className="min-h-screen ">
-        
         {children}
         <Toaster />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
